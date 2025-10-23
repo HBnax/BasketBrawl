2e34b044d5101994f2fb0126a0e3721712e11cf6
Halle Broadnax <broadnaxhalle@gmail.com>
2025-10-23 11:14:37 -0700

    Added Controls/Pause Screen

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,41 +1,91 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class UIController : MonoBehaviour
 {
     public GameObject startScreenPanel;
     public GameObject gameScreenPanel;
+    public GameObject controlsPanel;
+    public GameObject pausePanel;
+
+    public PlayerInput[] players;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        if (startScreenPanel)
-        {
-            startScreenPanel.SetActive(true);
-        }
-        
-        if (gameScreenPanel)
-        {
-            gameScreenPanel.SetActive(false);
-        }
+        SetActive(startScreenPanel, true);
+        SetActive(gameScreenPanel, false);
+        SetActive(controlsPanel, false);
+        SetActive(pausePanel, false);
+
+        EnablePlayers(false);
     }
 
     public void OnClickStart()
     {
-        if (startScreenPanel)
-        {
-            startScreenPanel.SetActive(false);
-        }
-        if (gameScreenPanel)
-        {
-            gameScreenPanel.SetActive(true);
-        }
+        SetActive(startScreenPanel, false);
+        SetActive(gameScreenPanel, true);
+        SetActive(controlsPanel, false);
+        SetActive(pausePanel, false);
+
+        EnablePlayers(true);
     }
 
+    public void OnClickOpenControls()
+    {
+        SetActive(startScreenPanel, false);
+        SetActive(gameScreenPanel, false);
+        SetActive(controlsPanel, true);
+        SetActive(pausePanel, false);
+
+        EnablePlayers(false);
+    }
+
+    public void OnClickCloseControls()
+    {
+        SetActive(startScreenPanel, true);
+        SetActive(gameScreenPanel,false);
+        SetActive(controlsPanel, false);
+        SetActive(pausePanel, false);
+        
+        EnablePlayers(false);
+    }
+
+    public void OnClickPause()
+    {
+        SetActive(pausePanel, true);
+        EnablePlayers(false);
+        Time.timeScale = 0f;
+    }
+
+    public void OnClickResume()
+    {
+        SetActive(pausePanel, false);
+        EnablePlayers(true);
+        Time.timeScale = 1f;
+    }
     public void OnClickExit()
     {
         Debug.Log("Exit Game");
         Application.Quit();
     }
 
+    void SetActive(GameObject go, bool isActive)
+    {
+        if (go) go.SetActive(isActive);
+    }
+
+    void EnablePlayers(bool isEnabled)
+    {
+        if (players == null) return;
+
+        foreach (var player in players)
+        {
+            if (player) player.enabled = isEnabled;
+        }
+            
+    }
+    
 }
