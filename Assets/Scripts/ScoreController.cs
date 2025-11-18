6fefa01d35dda24e98c3ea392e867a4dcbfc2872
Halle Broadnax <broadnaxhalle@gmail.com>
2025-11-18 15:20:13 -0800

    Update scene and create score class

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public enum Team { Team1, Team2 }
+
+    public int team1Score;
+    public int team2Score;
+    
+    
+}
