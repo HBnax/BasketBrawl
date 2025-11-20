c4ccb440c4d0a98222030f94283094cd0b35b2ba
Halle Broadnax <broadnaxhalle@gmail.com>
2025-11-20 11:06:45 -0800

    Update sensor logic

--- Assets/Scripts/HoopController.cs
+++ Assets/Scripts/HoopController.cs
@@ -3,48 +3,18 @@ using UnityEngine;
 
 public class HoopController : MonoBehaviour
 {
-    public enum Team { Team1, Team2 }
-
-    public Team pointsAwardedTo;
-
+    public ScoreController scoreController;
+    public ScoreController.Team pointsAwardedTo;
+    
     public BoxCollider2D rimLineSensor;
     public BoxCollider2D netSensor;
     public Rigidbody2D ball;
-    public ScoreController scoreController;
+    
     
     public float minDownwardVelocity = -0.05f;
 
     private bool crossedDown;
 
-    /*
-    private void FixedUpdate()
-    {
-        if (!ball) return;
-
-        Vector2 ballPosition = ball.position;
-        float ballYVelocity = ball.linearVelocity.y;
-
-        if (rimLineSensor && rimLineSensor.OverlapPoint(ballPosition))
-        {
-            crossedDown = true;
-            Debug.Log("Crossed Rim");
-        }
-
-        if (crossedDown && netSensor && netSensor.OverlapPoint(ballPosition))
-        {
-            crossedDown = false;
-            Debug.Log(pointsAwardedTo + " Scored");
-            //scoreController.AddPoint(pointsAwardedTo);
-        }
-
-        
-        if (crossedDown && rimLineSensor && ballPosition.y > rimLineSensor.bounds.max.y + 0.1f && ballYVelocity >= 0f)
-        {
-            crossedDown = false;
-        }
-        
-    }
-    */
     public void OnRimEnter(Collider2D obj)
     {
         var rb = obj.attachedRigidbody;
@@ -53,7 +23,6 @@ public class HoopController : MonoBehaviour
         if (ball.linearVelocity.y < minDownwardVelocity)
         {
             crossedDown = true;
-            Debug.Log("Crossed Rim");
         }
     }
     
@@ -73,8 +42,7 @@ public class HoopController : MonoBehaviour
 
         if (crossedDown)
         {
-            Debug.Log(pointsAwardedTo + " Scored");
-            //scoreController.AddPoint(pointsAwardedTo);
+            scoreController.AddPoint(pointsAwardedTo);
             crossedDown = false;
         }
     }
